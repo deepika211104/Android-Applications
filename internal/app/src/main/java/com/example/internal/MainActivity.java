24362386d95da32d1b6c3f434570206b57e0ab39
deepika <you@example.com>
2024-10-05 14:39:16 +0530

    proximity sensor check

---
+++ internal/app/src/main/java/com/example/internal/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.internal;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tv=(TextView) findViewById(R.id.sensor);
+        sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)!=null)
+        {
+            tv.setText("this device has proximity sensor");
+        }
+        else {
+            tv.setText("this device has no proximity sensor");
+        }
+    }
+}
